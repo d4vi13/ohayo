64743910d1ce80cee99b7620810e7030311f62ce
Davi Campos Ribeiro <dcr23@inf.ufpr.br>
2025-09-11 23:20:46 +0000

    refactor ohayo_write

--- driver/chrdev.c
+++ driver/chrdev.c
@@ -8,16 +8,14 @@ ohayo_write (struct file *f, const char __user *buf, size_t len, loff_t *offset)
 
   printk (KERN_INFO "escrevendo\n");
 
-  while (len - i > 4) 
+
+  for (; i < len/4; i++)
     {
       if ((*offset + i) > 0x1000)
         break;
 
-      writel (buffer[i/4], ctrl->mmio + *offset + i);
-      i += 4;
+      writel (buffer[i], ctrl->mmio + *offset + i);
     }
-
-  printk (KERN_INFO "primeiro elem: %x\n", buffer[0]);
 
   return i;
 }
@@ -33,13 +31,9 @@ ohayo_read (struct file *f, char __user *buf, size_t len, loff_t *offset)
   for (; i < len/4; i++)
     {
       if ((*offset + i) > 0x1000)
-      {
-        pr_err("out of bounds\n");
         break;
-      }
 
       buffer[i] = readl ( ctrl->mmio + *offset + i);
-      printk (KERN_INFO "valor lido (i=%ull): %x\n", i, buffer[0]);
     }
 
 
