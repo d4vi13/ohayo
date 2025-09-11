9b1a4476dbf2967709780556e9cc4cae5f42b4d4
Davi Campos Ribeiro <dcr23@inf.ufpr.br>
2025-09-11 16:43:07 +0000

    change printk to dev_info

--- driver/pci_handlers.c
+++ driver/pci_handlers.c
@@ -1,11 +1,41 @@
 #include "pci_handlers.h"
 
+static inline int 
+memory_setup (struct pci_dev *dev)
+{
+  /* Marca regiao como sendo utilizada dentro do kernel*/
+  err = pci_request_region (dev, 0, DRIVER_NAME);
+  if (err < 0) 
+    {
+      dev_err (&dev->dev,"requested memory region is busy\n");
+      return err;
+    }
+
+  dev_info (&dev->dev, "memory reserved\n");
+
+  /* Cria cookie da regiao mapeada.
+   * Essencialmente eh uma estrutura opaca
+   * que representa nosso bar mapeado em uma
+   * regiao de memoria virtual
+   */
+  ctrl->mmio = pci_iomap (dev, 0, 0x1000);
+  if (!ctrl->mmio)
+    {
+      dev_err (&dev->dev,"unable to memory map pci bar\n");
+      return -ENOMEM;
+    }
+
+  writel (0x0b0b, ctrl->mmio);
+  dev_info (&dev->dev, "valor lido %x\n", readl(ctrl->mmio));
+  dev_info (&dev->dev, "valor lido %x\n", readl(ctrl->mmio+4));
+
+  return 0;
+}
+
 int 
 ohayo_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
   int err = 0;
-  printk(KERN_INFO "dispositivo carregado\n");
-  printk(KERN_INFO "vendor: %x id: %x\n", id->vendor, id->device);
 
   /* Nesse ponto o BAR deve ter um endereco valido 
    * alocado anteriormente pelo kernel apos ter feito
@@ -21,33 +51,16 @@ ohayo_probe(struct pci_dev *dev, const struct pci_device_id *id)
     return err;
   }
 
-  printk(KERN_INFO "ohayo device enabled\n");
+  dev_info (&dev->dev, "device enabled\n");
 
-  /* Marca regiao como sendo utilizada dentro do kernel*/
-  err = pci_request_region (dev, 0, DRIVER_NAME);
-  if (err < 0) 
+  err = memory_setup (dev);
+  if (err < 0)
     {
-      dev_err (&dev->dev,"requested memory region is busy\n");
+      dev_err (&dev->dev, "unable to setup memory");
       return err;
     }
 
-  printk(KERN_INFO "memory reserved\n");
-
-  /* Cria cookie da regiao mapeada.
-   * Essencialmente eh uma estrutura opaca
-   * que representa nosso bar mapeado em uma
-   * regiao de memoria virtual
-   */
-  ctrl->mmio = pci_iomap (dev, 0, 0x1000);
-  if (!ctrl->mmio)
-    {
-      dev_err (&dev->dev,"unable to memory map pci bar\n");
-      return -ENOMEM;
-    }
-
-  writel (0x0b0b, ctrl->mmio);
-  printk (KERN_INFO "valor lido %x\n", readl(ctrl->mmio));
-  printk (KERN_INFO "valor lido %x\n", readl(ctrl->mmio+4));
+  dev_info (&dev->dev, "memory configured\n");
 
   return 0;
 }
